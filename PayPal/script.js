2c593c04ccc03a38b34d779b9bd05c565e55ddb8
Andrew Korshak <67147751+yakorshak@users.noreply.github.com>
2022-05-02 23:50:04 +0300

    uploaded

---
+++ PayPal/script.js
@@ -0,0 +1,24 @@
+const amountElement = document.getElementById('amount');
+
+console.log(amountElement.value);
+
+paypal.Buttons({
+    createOrder: function(data, actions) {
+      // This function sets up the details of the transaction, including the amount and line item details.
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: amountElement.value
+          }
+        }]
+      });
+    },
+    onApprove: function(data, actions) {
+      // This function captures the funds from the transaction.
+      return actions.order.capture().then(function(details) {
+        // This function shows a transaction success message to your buyer.
+        alert('Transaction completed by ' + details.payer.name.given_name);
+      });
+    }
+  }).render('#paypal');
+  //This function displays payment buttons on your web page.
